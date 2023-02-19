88965acbe547224616db2771127f346ece0fdf50
Viviana Scerbakova <125687697+vivianascerbakova221RDB309@users.noreply.github.com>
2023-02-19 16:32:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,19 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    
-    
-    for position, character in enumerate(text):
-        if character in "([{":
+    for i, next in enumerate(text):
+        if next in "([{":
             # Process opening bracket, write your code here
-            
-            opening_brackets_stack.append(Bracket(character, position + 1)) #bracket character = next; bracket position = position
-            pass
+            opening_brackets_stack.append(Bracket(next, i+1))
 
-        if character in ")]}":
 
-            
+        if next in ")]}":
             # Process closing bracket, write your code here
-            
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, character):
-                return position + 1
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
             opening_brackets_stack.pop()
-            
-    if opening_brackets_stack :
+
+    if opening_brackets_stack:
         return opening_brackets_stack[0].position
     return "Success"
 
